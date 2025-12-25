9c03f623c31018bd592da61c3a50b4a4711f8dd7
ganesh-srv <ganeshsrv.sarakadam@gmail.com>
2025-12-25 17:10:01 -0500

    added allow ports in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,23 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # WARNING: In production, restrict to your IP
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
